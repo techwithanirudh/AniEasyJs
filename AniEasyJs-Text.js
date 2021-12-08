b875bdf65db8d5c8518cdf3f5138820643123a7b
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-12-08 20:34:04 +0530

    Update AniEasyJs-Text.js

--- AniEasyJs-Text.js
+++ AniEasyJs-Text.js
@@ -181,3 +181,128 @@ function startsWith(str, prefix) {
 function endsWith(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
+
+// Add a function that is multiple ifs dynamically
+// (e.g. If( Condition1, ThenResult1 [, Condition2, ThenResult2, ... [ , DefaultResult ] ] ) => Result )
+function Ifs(condition, thenResult) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var result = thenResult;
+    for (var i = 0; i < args.length; i += 2) {
+        if (args[i] === condition) {
+            result = args[i + 1];
+            break;
+        }
+    }
+    return result;
+}
+
+// Add a function that is multiple ifs statically
+// (e.g. If( Condition1, ThenResult1 [, Condition2, ThenResult2, ... [ , DefaultResult ] ] ) => Result )
+function If(condition, thenResult, elseResult) {
+    return Ifs(condition, thenResult, elseResult);
+}
+
+// Add a function to check if a string is a number
+// (e.g. isNumber('foo') => false)
+function isNumber(str) {
+    return !isNaN(str);
+}
+
+// Add a function to get local storage
+// (e.g. getLocalStorage('foo') => 'bar')
+function getLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
+// Add a function to set local storage
+// (e.g. setLocalStorage('foo', 'bar') => 'bar')
+function setLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+// Add a function to remove local storage
+// (e.g. removeLocalStorage('foo') => null)
+function removeLocalStorage(key) {
+    localStorage.removeItem(key);
+}
+
+// Add a function to get session storage
+// (e.g. getSessionStorage('foo') => 'bar')
+function getSessionStorage(key) {
+    return sessionStorage.getItem(key);
+}
+
+// Add a function to set session storage
+// (e.g. setSessionStorage('foo', 'bar') => 'bar')
+function setSessionStorage(key, value) {
+    sessionStorage.setItem(key, value);
+}
+
+// Add a function to remove session storage
+// (e.g. removeSessionStorage('foo') => null)
+function removeSessionStorage(key) {
+    sessionStorage.removeItem(key);
+}
+
+// Add a function to get the current url
+// (e.g. getUrl() => 'http://www.google.com')
+function getUrl() {
+    return window.location.href;
+}
+
+// Add a function to get the current host
+// (e.g. getHost() => 'www.google.com')
+function getHost() {
+    return window.location.host;
+}
+
+// Add a function to get the current protocol
+// (e.g. getProtocol() => 'http')
+function getProtocol() {
+    return window.location.protocol;
+}
+
+// Add a function to get the current path
+// (e.g. getPath() => '/foo/bar')
+function getPath() {
+    return window.location.pathname;
+}
+
+// Add a function to get the current search
+// (e.g. getSearch() => '?foo=bar')
+function getSearch() {
+    return window.location.search;
+}
+
+// Add a function to get the current hash
+// (e.g. getHash() => '#foo')
+function getHash() {
+    return window.location.hash;
+}
+
+// Add a function to get the current port
+// (e.g. getPort() => '80')
+function getPort() {
+    return window.location.port;
+}
+
+// Add a function to get the position of text
+// (e.g. getPosition('foo', 'bar') => 3)
+function getPosition(str, find) {
+    return str.indexOf(find);
+}
+
+// Add a function to get the substring of text
+// (e.g. getSubstring('foo', 1, 3) => 'oo')
+function getSubstring(str, start, end) {
+    return str.substring(start, end);
+}
+
+// Add a function to get the sum of numbers dynamically
+// (e.g. sum(1, 2, 3) => 6)
+function sum(num1, num2, num3) {
+    var args = Array.prototype.slice.call(arguments);
+    return args.reduce(function(a, b) {
+        return a + b;
+    });
+}
